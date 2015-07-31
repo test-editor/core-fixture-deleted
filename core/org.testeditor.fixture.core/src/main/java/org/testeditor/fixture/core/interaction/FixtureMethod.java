da370abb5a434040fe4efff34d1429eeec334c26
Franz Becker <franz.becker@itemis.de>
2015-07-31 15:17:01 +0200

    [TE-241] remove unused import

--- core/org.testeditor.fixture.core/src/main/java/org/testeditor/fixture/core/interaction/FixtureMethod.java
+++ core/org.testeditor.fixture.core/src/main/java/org/testeditor/fixture/core/interaction/FixtureMethod.java
@@ -13,7 +13,6 @@ package org.testeditor.fixture.core.interaction;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
